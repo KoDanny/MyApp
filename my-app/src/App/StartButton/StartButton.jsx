356a13a54076aa27ab2331e8bd87398788621cb3
Komogorov Denis <komogorov.denis1990@bk.ru>
2025-01-02 12:46:50 +0300

    remove files

--- my-app/src/App/StartButton/StartButton.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './StartButton.module.css';
-
-export const StartButton = ({ onClick }) => (
-	<button className={styles.btn} onClick={onClick}>
-		<span className={styles.text}>Начать заново</span>
-	</button>
-);
