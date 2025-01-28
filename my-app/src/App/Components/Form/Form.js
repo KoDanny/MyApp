30e21d1edc1e55ecaf0d42845f2291d3c3cb7220
Komogorov Denis <komogorov.denis1990@bk.ru>
2025-01-28 16:46:20 +0300

    remove files

--- my-app/src/App/Components/Form/Form.js
+++
@@ -1,5 +0,0 @@
-import { FormLayout } from './Layout/FormLayout';
-
-export const Form = (props) => {
-	return <FormLayout {...props} />;
-};
--- my-app/src/App/Components/index.js
+++
@@ -1,4 +0,0 @@
-export * from './Task/Layout/TaskLayout';
-export * from './TaskList/TaskList';
-export * from './Loader/Loader';
-export * from './Form/Form';
